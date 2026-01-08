e07006eeda6767183558aa66d09d4634c8407ddb
esculapweb <esculapweb@gmail.com>
2026-01-08 03:53:13 +0300

    drawer navigation

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,16 +1,18 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem  } from "@react-navigation/drawer";
 import { useContext } from "react";
+import { Ionicons } from '@expo/vector-icons';
+
 import WelcomeScreen from "../screens/WelcomeScreen";
 import Profile from "../screens/Profile";
-import IconButton from "../components/ui/IconButton";
 import { AuthContext } from "../store/auth-context";
 import { Colors } from "../constants/styles";
-import { Platform } from "react-native";
+import { View, Text} from "react-native";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
-  const authCtx = useContext(AuthContext);
   return (
     <Stack.Navigator
       screenOptions={{
@@ -22,26 +24,62 @@ const AppStack = () => {
       }}
     >
       <Stack.Screen
-        name="Profile"
+        name="ProfileScreen"
         component={Profile}
         options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="log-out-outline"
-              color={Platform.select({
-                ios: Colors.primary500,
-                android: Colors.primary100,
-              })}
-              size={24}
-              onPress={authCtx.logout}
-              style={{ marginTop: -2 }}
-            />
-          ),
+          headerShown: false
         }}
-      />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      />  
     </Stack.Navigator>
   );
 };
 
-export default AppStack;
+
+function CustomDrawerContent(props) {
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {/* <View >
+        <Text >Denis</Text>
+      </View> */}
+
+      <DrawerContentScrollView {...props} contentContainerStyle={{ flexGrow: 1 }}>
+        <DrawerItemList {...props} />
+        <View style={{ flex: 1 }} />
+        <DrawerItem
+          label="Logout"
+          labelStyle={{ color: Colors.primary500 }}
+          onPress={authCtx.logout}
+          icon={({color, size}) => (
+            <Ionicons name="log-out-outline" color={color} size={size} />
+          )}
+        />
+      </DrawerContentScrollView>
+    </View>
+  );
+}
+
+
+const AppDrawer = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: Colors.primary100,
+        },
+        drawerActiveTintColor: Colors.primary500,
+      }}
+    >
+      <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+      <Drawer.Screen
+        name="Profile"
+        component={AppStack}
+        options={{ title: "Profile" }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default AppDrawer;
--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -3,13 +3,13 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { AuthContext } from "../store/auth-context";
 import AuthStack from "./AuthStack";
-import AppStack from "./AppStack";
+import AppDrawer from "./AppStack";
 
 const Navigation = () => {
   const authCtx = useContext(AuthContext);
   return (
     <NavigationContainer>
-      {authCtx.isAuthenticated ? <AppStack /> : <AuthStack />}
+      {authCtx.isAuthenticated ? <AppDrawer /> : <AuthStack />}
     </NavigationContainer>
   );
 };
