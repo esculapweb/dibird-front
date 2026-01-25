1187bd73fde3121106d9895f4b8d4b116b917dda
esculapweb <esculapweb@gmail.com>
2026-01-26 02:16:14 +0300

    locales

---
+++ i18next-parser.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  locales: ['en', 'ru'],
+  output: 'locales/$LOCALE.json',
+  input: [
+    'components/**/*.{js,ts,jsx,tsx}',
+    'navigation/**/*.{js,ts,jsx,tsx}',
+    'screens/**/*.{js,ts,jsx,tsx}',
+    'services/**/*.{js,ts,jsx,tsx}',
+    'store/**/*.{js,ts,jsx,tsx}'
+  ],
+  sort: true,
+  verbose: true,
+  lexers: {
+    js: ['JavascriptLexer'],
+    ts: ['JavascriptLexer'],
+    jsx: ['JsxLexer'],
+    tsx: ['JsxLexer']
+  }
+};
