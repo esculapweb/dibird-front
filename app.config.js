c7d29cfa523e912d4f3b8f8384b5eb484aa05b74
esculapweb <esculapweb@gmail.com>
2026-06-29 17:50:51 +0300

    ver updated

--- app.config.js
+++ app.config.js
@@ -27,7 +27,7 @@ export default {
     name: variant.name,
     slug: "dibird",
     owner: "esculapweb",
-    version: "26.06.1",
+    version: "26.06.2",
     orientation: "portrait",
     scheme: "dibird",
     userInterfaceStyle: "automatic",
